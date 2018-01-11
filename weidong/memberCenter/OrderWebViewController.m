496868bee96b9c8d318218994666ca5d9569e844
zhccc <332606039@qq.com>
2018-01-12 07:05:17 +0800

    修改版本号

--- weidong/memberCenter/OrderWebViewController.m
+++ weidong/memberCenter/OrderWebViewController.m
@@ -60,7 +60,7 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-    self.navigationController.navigationBar.hidden = YES;
+//    self.navigationController.navigationBar.hidden = YES;
     
     if (![Util userIsLogin]) {
         [self openLoginVC];
